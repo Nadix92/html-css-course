0e2cf6f4f3e0fffad88575137632d8b59b8b56bd
Nadix92 <49558241+Nadix92@users.noreply.github.com>
2021-11-08 14:27:01 +0100

    Optimized page ready for deplyment

--- starter/07-Omnifood-Desktop/js/script.js
+++ starter/07-Omnifood-Desktop/js/script.js
@@ -1,4 +1,58 @@
 ///////////////////////////////////////////////////////////
+// Fixing scrolling animation
+const allLink = document.querySelectorAll('a:link');
+const headerEl = document.querySelector('.header');
+
+allLink.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+
+    // Scroll back to top
+    if (href === '#') window.scrollTo({ top: 0, behavior: 'smooth' });
+
+    // Scroll to spesific section
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    // close mobile navigation
+    if (link.classList.contains('main-nav-link')) headerEl.classList.toggle('nav-open');
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Sticky Navigation
+const heroSection = document.getElementById('hero');
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) document.body.classList.add('sticky');
+    else document.body.classList.remove('sticky');
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: '-80px',
+  }
+);
+obs.observe(heroSection);
+
+///////////////////////////////////////////////////////////
+// get and set current year in footer copy right
+document.getElementById('year').textContent = new Date().getFullYear();
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+const btnNav = document.querySelector('.btn-mobile-nav');
+btnNav.addEventListener('click', () => {
+  headerEl.classList.toggle('nav-open');
+});
+
+///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement('div');
@@ -17,14 +71,3 @@ function checkFlexGap() {
   if (!isSupported) document.body.classList.add('no-flexbox-gap');
 }
 checkFlexGap();
-
-// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
-
-//  get and set current year in footer copy right
-document.getElementById('year').textContent = new Date().getFullYear();
-
-// Make mobile navigation work
-const btnNav = document.querySelector('.btn-mobile-nav');
-btnNav.addEventListener('click', () => {
-  document.querySelector('.header').classList.toggle('nav-open');
-});
